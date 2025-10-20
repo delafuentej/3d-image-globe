2a62f42234a51f08673013f571373ceec6af002f
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-10-20 17:01:26 +0200

    tailwindcss bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div id="navbar" className="font-orbitron">
-      3D IMAGE GLOBE
+      <h1>3D IMAGE GLOBE</h1>
     </div>
   );
 };
